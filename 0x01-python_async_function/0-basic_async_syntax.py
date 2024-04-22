84971db89df8cc6ad1e8502198b06609cdaec853
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-22 13:24:13 +0300

    Added Docs to task 0 and 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Task 0"""
+
 import random
 import time
 
 
 async def wait_random(max_delay=10.0):
+    """Function that waits a random amount of time"""
     duration = random.uniform(0, max_delay)
     time.sleep(duration)
     return duration
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Task 1"""
+
 import random
 import time
 
@@ -6,6 +8,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n, max_delay=10.0):
+    """Function that waits a random amount of time"""
     durations = []
     for _ in range(n):
         duration = random.uniform(0, max_delay)
