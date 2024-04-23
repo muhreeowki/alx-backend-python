61de8ea0ae6999ab773b7ed289ebb2e847a6cafa
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-23 21:35:05 +0300

    Fixing task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,8 @@
 
 import asyncio
 from typing import List
-from importlib import import_module as using
 
-async_generator = using("./0-async_generator.py").async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
