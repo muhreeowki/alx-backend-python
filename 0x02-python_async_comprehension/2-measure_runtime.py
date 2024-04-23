879e8bb0abdebdb2ef3b6d75863c10f5779dece9
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-23 21:46:39 +0300

    Added task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,13 +2,12 @@
 """Task 2 Module"""
 
 import asyncio
-from typing import List
 import time
 
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Task 2 Function"""
     start = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
