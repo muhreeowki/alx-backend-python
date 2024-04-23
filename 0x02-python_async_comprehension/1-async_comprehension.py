648b6779a430e614e7fbba3eb98ba5c95726694d
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-23 21:25:10 +0300

    Added task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Task 1 Module"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__("./0-async_generator.py").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Task 1 Function"""
+    return [num async for num in async_generator()]
