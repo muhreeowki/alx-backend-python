3936485c16f884060af127dc09dceffdb702d6d5
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-23 21:14:20 +0300

    Fixing task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Task 0 coroutine"""
+async def async_generator() -> Generator[float, None, None]:
+    """Generates a sequence of 10 numbers."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
