30c7c276b5831aed35d98ad4286912e6d3b91d6c
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-23 05:36:58 +0300

    Fixing task 0 and 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Task 0"""
-
-import random
+'''Task 0's module.
+'''
 import asyncio
+import random
 
 
-async def wait_random(max_delay=10.0):
-    """Function that waits a random amount of time"""
-    duration = random.uniform(0, max_delay)
-    await asyncio.sleep(duration)
-    return duration
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""Task 1"""
-
-import random
+'''Task 1's module.
+'''
 import asyncio
+from typing import List
 
-wait_random = __import__("0-basic_async_syntax").wait_random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay=10.0):
-    """Function that waits a random amount of time"""
-    durations = []
-    for _ in range(n):
-        duration = random.uniform(0, max_delay)
-        await asyncio.sleep(duration)
-        durations.append(duration)
-    return durations
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
